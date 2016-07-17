442d90c9a9a38ae0356e29e2eb0b25fbd024f4c6
israel parra conejero <israel parra conejero>
2016-07-17 19:06:00 +0200

    ps4 replace custom autoload
    added php-di, aura-session and doctrine

--- public/index.php
+++ public/index.php
@@ -1,21 +1,19 @@
 <?php
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 //directorio del proyecto
 define("PROJECTPATH", dirname(__DIR__));
 
 //directorio app
 define("APPPATH", PROJECTPATH . '/App');
 
-//autoload con namespaces
-function autoload_classes($class_name)
-{
-    $filename = PROJECTPATH . '/' . str_replace('\\', '/', $class_name) .'.php';
-    if(is_file($filename))
-    {
-        include_once $filename;
-    }
-}
-//registramos el autoload autoload_classes
-spl_autoload_register('autoload_classes');
+
+require __DIR__ . '/../vendor/autoload.php';
 
 //instancia de la app
 $app = new \Core\App;
